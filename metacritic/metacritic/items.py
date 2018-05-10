807df907c2510f03b2790c2b50d439d4b9fcf80b
jvichare <joshvichare@gmail.com>
2018-05-10 11:45:48 -0400

    defined items.py for scraped fields

--- metacritic/metacritic/items.py
+++ metacritic/metacritic/items.py
@@ -9,6 +9,15 @@ import scrapy
 
 
 class MetacriticItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    movie = scrapy.Field()
+    release_date = scrapy.Field()
+    genre = scrapy.Field()
+    metascore = scrapy.Field()
+    user_score = scrapy.Field()
+    username = scrapy.Field()
+    user_type = scrapy.Field()
+    review_score = scrapy.Field()
+    review = scrapy.Field()
+
+
+
