ccf78ae982e3195c9f35d5fbcc6f1d1e00f13ab8
jvichare <joshvichare@gmail.com>
2018-05-11 12:00:20 -0400

    successfully passed the test of getting all user and critic reviews

--- metacritic/metacritic/spiders/metacritic_spider.py
+++ metacritic/metacritic/spiders/metacritic_spider.py
@@ -43,8 +43,9 @@ class MetacriticSpider(Spider):
         release_date = response.xpath('//span[@class="release_date"]/span[2]/text()').extract_first()
         genre_list = response.xpath('//div[@class="genres"]/span[2]').extract() # have to fix this list
         scores = response.xpath('//a[@class="metascore_anchor"]/div/text()').extract()
-        metascore = int(scores[0])
-        user_score = float(scores[1])
+        metascore = int(scores[0]) # integer from 0 -> 100
+        user_score = float(scores[1]) # number between 0 -> 10, that is why I use 
+                                      # float() instead of int()
 
         # cleaning up the scraped list of genres
         genre_string = ''.join(genre_list)
@@ -70,7 +71,8 @@ class MetacriticSpider(Spider):
                                                     'release_date': release_date,
                                                     'genre': genre,
                                                     'metascore': metascore,
-                                                    'user_score': user_score}, callback=self.parse_user_reviews)
+                                                    'user_score': user_score,
+                                                    'num_user_reviews': num_user_reviews}, callback=self.parse_user_reviews)
 
     def parse_critic_reviews(self, response):
         """
@@ -83,47 +85,120 @@ class MetacriticSpider(Spider):
         metascore = response.meta['metascore']
         user_score = response.meta['user_score']
 
-        # Getting the review attributes
+        # Getting the review attributes in a list
         reviews = response.xpath('//div[@class="critic_reviews"]//a[@class="no_hover"]/text()').extract()
         usernames = response.xpath('//span[@class="author"]/a/text()').extract()
         review_scores = response.xpath('//div[@class="left fl"]/div/text()').extract()
+
+        # Singular attribute, not going to change within this function
         user_type = "critic"
 
         # Writing the fields to the metacritic item
         item = MetacriticItem()
 
-        item['movie'] = movie
-        item['release_date'] = release_date
-        item['genre'] = genre
-        item['metascore'] = metascore
-        item['user_score'] = user_score
-        item['user_type'] = user_type
+        # Have to loop the number of critic reviews
+        for i in list(range(0, len(usernames) - 1)):
+            item['movie'] = movie
+            item['release_date'] = release_date
+            item['genre'] = genre
+            item['metascore'] = metascore
+            item['user_score'] = user_score
+            item['user_type'] = user_type
 
-        # have to loop over the reviews, usernames, and review_scores list to write them into the object
-        for review in reviews:
-            item['review'] = review.strip()
+            item['review'] = reviews[i].strip()
+            item['username'] = usernames[i]
+            item['review_score'] = int(review_scores[i])
 
-        for username in usernames:
-            item['username'] = username
-
-        for review_score in review_scores:
-            item['review_score'] = int(review_score)
-
-        yield item
+            yield item
 
     def parse_user_reviews(self, response):
         """
         This function grabs the name of each user, their individual score, and their entire review.
+        """        
+        # Calling back the previous meta attributes
+        movie = response.meta['movie']
+        release_date = response.meta['release_date']
+        genre = response.meta['genre']
+        metascore = response.meta['metascore']
+        user_score = response.meta['user_score']
+
+        # Singular attribute, not going to change within the context of this function
+        user_type = "user"
+
+        # User reviews and score attributes
+        reviews = user_revews = response.xpath('//span[@class="blurb blurb_expanded"]/text()').extract()
+        usernames = response.xpath('//span[@class="author"]/a/text()').extract()
+        review_scores = response.xpath('//div[@class="left fl"]/div/text()').extract()
+
+        item = MetacriticItem()
+
+        for i in list(range(0, len(usernames) - 1)):
+            item['movie'] = movie
+            item['release_date'] = release_date
+            item['genre'] = genre
+            item['metascore'] = metascore
+            item['user_score'] = user_score
+            item['user_type'] = user_type
+
+            item['review'] = reviews[i].strip()
+            item['username'] = usernames[i]
+            item['review_score'] = float(review_scores[i]) # they are a decimal for now,
+                                                           # will convert during cleanup
+
+            yield item
+        
+        # There are most certainly more pages (100 comments per page), getting pattern
+        # of the following urls
+        num_user_reviews = response.meta['num_user_reviews']
+        rev_per_page = 100
+
+        num_pages = num_user_reviews // rev_per_page # not adding one since metacritic actually
+                                                     # indexes their successive review pages
+                                                     # starting from 0 (i.e. page 1 = '?page=0')
+
+        # starting the range at 1 because we already scraped the first page ('page=0') of reviews
+        following_urls = [response.url + '?page={}'.format(x) for x in range(1, num_pages + 1)] 
+
+        for url in following_urls:
+            yield Request(url=url, meta={'movie': movie,
+                                            'release_date': release_date,
+                                            'genre': genre,
+                                            'metascore': metascore,
+                                            'user_score': user_score}, callback=self.parse_following_review_page)
+
+    def parse_following_review_page(self, response):
         """
-        # reviews = 
-        # usernames = response.xpath('//span[@class="author"]/a/text()').extract()
-        # review_scores = response.xpath('//div[@class="left fl"]/div/text()').extract()
-        # user_type = "user"
+        This function is the same as the parsing of the user reviews page, ignoring the multiple
+        requests for the following review pages.
+        """        
+        # Calling back the previous meta attributes
+        movie = response.meta['movie']
+        release_date = response.meta['release_date']
+        genre = response.meta['genre']
+        metascore = response.meta['metascore']
+        user_score = response.meta['user_score']
 
-        # item = MetacriticItem()
+        # Singular attribute, not going to change within the context of this function
+        user_type = "user"
 
-        # item['username'] = 
-        # item['user_type'] = user_type
-        # item['review_score'] = 
-        # item['review'] = 
-        pass
+        # User reviews and score attributes
+        reviews = user_revews = response.xpath('//span[@class="blurb blurb_expanded"]/text()').extract()
+        usernames = response.xpath('//span[@class="author"]/a/text()').extract()
+        review_scores = response.xpath('//div[@class="left fl"]/div/text()').extract()
+
+        item = MetacriticItem()
+
+        for i in list(range(0, len(usernames) - 1)):
+            item['movie'] = movie
+            item['release_date'] = release_date
+            item['genre'] = genre
+            item['metascore'] = metascore
+            item['user_score'] = user_score
+            item['user_type'] = user_type
+
+            item['review'] = reviews[i].strip()
+            item['username'] = usernames[i]
+            item['review_score'] = float(review_scores[i]) # they are a decimal for now,
+                                                           # will convert during cleanup
+
+            yield item
